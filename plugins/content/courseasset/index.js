ab127e04a245cede888b95850e3cdf7b4202cd00
Thomas Taylor <thomas.taylor@kineo.com>
2017-09-15 13:56:10 +0100

    Fix courseasset destroy contentHook

--- plugins/content/courseasset/index.js
+++ plugins/content/courseasset/index.js
@@ -49,109 +49,102 @@ function initialize() {
             logger.log('err', err);
             return next(err);
           }
-          if (items && items.length == 1) {
-            var itemForDeletion = items[0].toObject();
-            var criteria = {};
-
-            async.series([
-              function(callback){
-                if (contentType !== 'contentobject') {
-                  return callback(null, 'Not processing a content object');
-                }
-                db.retrieve('article', { _courseId: itemForDeletion._courseId, _parentId: itemForDeletion._id }, function(err, articles) {
-                  if (err) {
-                    return callback(err, 'Unable to retrieve child articles of contentobject ' + itemForDeletion._id);
-                  }
-                  var parentIds = [];
-                  if (articles && articles.length !== 0) {
-                    criteria._courseId = itemForDeletion._courseId;
-                    parentIds = _.pluck(articles, '_id');
-                  }
-                  async.each(articles, function(article, cb) {
-                    db.retrieve('block', {_courseId: article._courseId, _parentId: article._id}, function(err, blocks) {
-                      if (err) {
-                        return callback(err, 'Unable to retrieve child blocks of article ' + article._id);
-                      }
-                      if (blocks && blocks.length !== 0) {
-                        parentIds = parentIds.concat(_.pluck(blocks, '_id'));
-                      }
-                      if(parentIds.length > 0) {
-                        criteria.$or = [
-                          { _contentTypeParentId: { $in: parentIds } },
-                          { _contentTypeId: itemForDeletion._id }
-                        ];
-                      } else {
-                        criteria._contentTypeId = itemForDeletion._id;
-                      }
-                      callback(null, 'Child content objects added to criteria object');
-                    });
-                  });
-                });
-              },
-              function(callback){
-                if (contentType !== 'article') {
-                  return callback(null, 'Not processing an article');
-                }
-                // When processing an article we need to retrieve the blocks in order to remove the associated assets.
-                db.retrieve('block', {_courseId: itemForDeletion._courseId, _parentId: itemForDeletion._id}, function(err, blocks) {
-                  if (err) {
-                    return callback(err, 'Unable to retrieve child blocks of article ' + itemForDeletion._id);
-                  }
-                  // Formulate the block search criteria.
-                  if (blocks && blocks.length !== 0) {
-                    // We have enough information to start removing course assets.
-                    criteria._contentTypeParentId  = { $in: [_.pluck(blocks, '_id')] };
-                    criteria._courseId = itemForDeletion._courseId;
-                  }
-                  callback(null, 'Blocks added to criteria object');
-                });
-              },
-              function(callback){
-                // Build the search criteria for the delete.
-                switch (contentType) {
-                  // NOTE: 'article' is a special case -- see code above.
-                  case 'course':
-                    criteria._courseId = itemForDeletion._id;
-                    break;
-                  case 'block':
-                    criteria._courseId = itemForDeletion._courseId;
-                    criteria._contentTypeParentId = itemForDeletion._id;
-                    criteria._contentType = 'component';
-                    break;
-                  case 'component':
-                    criteria._courseId = itemForDeletion._courseId;
-                    criteria._contentTypeId = itemForDeletion._id;
-                    criteria._contentType = 'component';
-                    break;
-                }
-                callback(null, 'Search criteria successfully set up');
-              }
-            ],
-            // Callback to here
-            function(err, results){
-              if (err) {
-                logger.log('error', err);
-                return next(err);
-              }
-              // Only remove the courseasset if there is enough critera to search on.
-              if (Object.keys(criteria).length !== 0) {
-                db.destroy('courseasset', criteria, function (err) {
-                  if (err) {
-                    logger.log('error', err);
-                    return next(err);
-                  }
-                  next(null, data);
-                });
-              } else {
-                // There is not enough criteria to delete assets.
-                // (This is most likely because an article with no block was removed).
-                next(null, data);
-              }
-            });
-          } else {
+          if (items && items.length !== 1) {
             logger.log('info', 'Unexpected number of ' + contentType + ' items found in courseasset pre-delete hook');
             next(null, data);
           }
+          var itemForDeletion = items[0].toObject();
+          var criteria = {};
+
+          async.series([
+            function(callback){
+              if (contentType !== 'contentobject') {
+                return callback(null, 'Not processing a content object');
+              }
+              db.retrieve('article', { _courseId: itemForDeletion._courseId, _parentId: itemForDeletion._id }, function(err, articles) {
+                if (err) {
+                  return callback(err, 'Unable to retrieve child articles of contentobject ' + itemForDeletion._id);
+                }
+                var parentIds = [];
+                if (articles && articles.length !== 0) {
+                  criteria._courseId = itemForDeletion._courseId;
+                  parentIds = _.pluck(articles, '_id');
+                }
+                async.each(articles, function(article, cb) {
+                  db.retrieve('block', {_courseId: article._courseId, _parentId: article._id}, function(err, blocks) {
+                    if (err) {
+                      return cb(err, 'Unable to retrieve child blocks of article ' + article._id);
+                    }
+                    if (blocks && blocks.length !== 0) {
+                      parentIds = parentIds.concat(_.pluck(blocks, '_id'));
+                    }
+                    if(parentIds.length > 0) {
+                      criteria.$or = [
+                        { _contentTypeParentId: { $in: parentIds } },
+                        { _contentTypeId: itemForDeletion._id }
+                      ];
+                    } else {
+                      criteria._contentTypeId = itemForDeletion._id;
+                    }
+                    cb(null, 'Child content objects added to criteria object');
+                  });
+                }, callback);
+              });
+            },
+            function(callback){
+              if (contentType !== 'article') {
+                return callback(null, 'Not processing an article');
+              }
+              // When processing an article we need to retrieve the blocks in order to remove the associated assets.
+              db.retrieve('block', {_courseId: itemForDeletion._courseId, _parentId: itemForDeletion._id}, function(err, blocks) {
+                if (err) {
+                  return callback(err, 'Unable to retrieve child blocks of article ' + itemForDeletion._id);
+                }
+                // Formulate the block search criteria.
+                if (blocks && blocks.length !== 0) {
+                  // We have enough information to start removing course assets.
+                  criteria._contentTypeParentId  = { $in: [_.pluck(blocks, '_id')] };
+                  criteria._courseId = itemForDeletion._courseId;
+                }
+                callback(null, 'Blocks added to criteria object');
+              });
+            },
+            function(callback){
+              // Build the search criteria for the delete.
+              switch (contentType) {
+                // NOTE: 'article' is a special case -- see code above.
+                case 'course':
+                  criteria._courseId = itemForDeletion._id;
+                  break;
+                case 'block':
+                  criteria._courseId = itemForDeletion._courseId;
+                  criteria._contentTypeParentId = itemForDeletion._id;
+                  criteria._contentType = 'component';
+                  break;
+                case 'component':
+                  criteria._courseId = itemForDeletion._courseId;
+                  criteria._contentTypeId = itemForDeletion._id;
+                  criteria._contentType = 'component';
+                  break;
+              }
+              callback(null, 'Search criteria successfully set up');
+            }
+          ], function(err, results){
+            if (err) {
+              logger.log('error', err);
+              return next(err);
+            }
+            // Only remove the courseasset if there is enough critera to search on.
+            if (Object.keys(criteria).length === 0) {
+              // There is not enough criteria to delete assets.
+              // (This is most likely because an article with no block was removed).
+              return next(null, data);
+            }
+            db.destroy('courseasset', criteria, function (err) {
+              if (err) logger.log('error', err);
+              next(err, data);
+            });
+          });
         });
       });
     }.bind(null, contentType));
